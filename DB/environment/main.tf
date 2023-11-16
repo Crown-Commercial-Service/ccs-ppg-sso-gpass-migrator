c8b87f8870e550da58d4259f48ae348f73d002f6
ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2023-11-17 00:31:47 +0530

    enbale backend

--- DB/environment/main.tf
+++ DB/environment/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "ppgnft-backend-store"
+    encrypt        = true
+    key            = "postgres-migrator/terraform.tfstate"
+    region         = "eu-west-2"
+    profile        = "nft"
+    #dynamodb_table = "terraform-state-lock-dynamo"
+  }
+}
+
 provider "aws" {
     region = "eu-west-2"
 
