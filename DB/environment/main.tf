d169da10bf7c729c6beb4db1db266186dd4dc33d
ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2023-11-22 16:25:51 +0530

    db migration code fixed

--- DB/environment/main.tf
+++ DB/environment/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "ppgnft-backend-store"
+    bucket         = "ppgpreprod-backend-store"
     encrypt        = true
     key            = "postgres-migrator/terraform.tfstate"
     region         = "eu-west-2"
-    profile        = "nft"
+    profile        = "preprod"
     #dynamodb_table = "terraform-state-lock-dynamo"
   }
 }
@@ -12,5 +12,5 @@ terraform {
 provider "aws" {
     region = "eu-west-2"
 
-    profile = "nft"
+    profile = "preprod"
 }
